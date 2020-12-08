27e892b22b83232dd9d7ea9bed04ea485bc4a6c4
Leah Rivkin <lrivkin@atlassian.com>
2020-12-08 14:40:54 -0800

    Day 7 part 2

--- day7/handy_haversacks.py
+++ day7/handy_haversacks.py
@@ -54,11 +54,33 @@ def run(file, bag):
         [bag_dict.pop(b) for b in to_search_next]
         to_search = to_search_next.copy()
 
-    # print '\nThis bag ({}) can be carried in: {}'.format(bag, can_carry_in)
-    print '\nThis bag ({}) can be carried in {} other bags'.format(bag, len(can_carry_in))
+    print 'This bag ({}) can be carried in {} other bags'.format(bag, len(can_carry_in))
     return len(can_carry_in)
 
 
+def count_num_bags_inside(bag_dict, bag):
+    this_bag = bag_dict.get(bag, None)
+    if not this_bag:
+        # This bag has no other bags inside
+        return 0
+
+    num_this_bag_can_hold = 0
+
+    for b in this_bag:
+        num_b_in_this_bag = this_bag[b]
+        # print '{} bag has {} {} bags'.format(bag, b, num_b_in_this_bag)
+        num_b_can_hold = count_num_bags_inside(bag_dict, b)
+        num_this_bag_can_hold += num_b_in_this_bag * (1 + num_b_can_hold)
+    # print '{} bag can overall hold {}'.format(bag, num_this_bag_can_hold)
+    return num_this_bag_can_hold
+
+
 if __name__ == '__main__':
+    # Part 1
     assert run('testinput.txt', 'shiny gold') == 4
     run('input.txt', 'shiny gold')
+
+    # Part 2
+    assert count_num_bags_inside(parse_input('part2test.txt'), 'shiny gold') == 126
+    assert count_num_bags_inside(parse_input('testinput.txt'), 'shiny gold') == 32
+    print count_num_bags_inside(parse_input('input.txt'), 'shiny gold')
