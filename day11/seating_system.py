a3dc54a42f651f91a272e73646c9a8e6d176fa16
Leah Rivkin <lrivkin@atlassian.com>
2020-12-11 11:57:11 -0800

    finished day 11, made some optimizations to use same code for part 1/2

--- day11/seating_system.py
+++ day11/seating_system.py
@@ -2,43 +2,45 @@ num_rows = 0
 num_cols = 0
 
 
-def count_occupied(board, position):
+def count_visible(board, position, max_depth=None):
     c = position % num_cols
     r = position / num_cols
+    directions = [(-1,-1), (-1,0), (-1,+1), (0,-1), (0,+1), (+1,-1), (+1,0), (+1,+1)]
 
-    # check all (max 8) surrounding spots
-    checks = []
-    if c > 0 and r > 0:
-        checks.append(position - num_cols - 1)
-    if r > 0:
-        checks.append(position - num_cols)
-    if r > 0 and c < num_cols - 1:
-        checks.append(position - num_cols + 1)
-    if c > 0:
-        checks.append(position - 1)
-    if c < num_cols - 1:
-        checks.append(position + 1)
-    if r < num_rows - 1 and c > 0:
-        checks.append(position + num_cols - 1)
-    if r < num_rows - 1:
-        checks.append(position + num_cols)
-    if (r < num_rows - 1) and (c < num_cols - 1):
-        checks.append(position + num_cols + 1)
+    num_occupied = 0
 
-    occupied = 0
-    for p in checks:
-        if board[p] == '#':
-            occupied += 1
-    return occupied
+    for (dr, dc) in directions:
+        d = 0   # used to limit how far we search
+        r0 = r
+        c0 = c
+        while 1:
+            r0 += dr
+            c0 += dc
+            if not(0 <= r0 < num_rows and 0 <= c0 < num_cols):
+                # We reached the edge of the grid
+                break
+            if board[r0*num_cols+c0] == 'L':
+                break
+            if board[r0*num_cols+c0] == '#':
+                num_occupied += 1
+                break
+
+            if max_depth:
+                # Only search x number of chairs around you
+                d += 1
+                if d == max_depth:
+                    break
+    return num_occupied
 
 
 def pretty_print(board, cols):
     chunks = [board[i:i + cols] for i in range(0, len(board), cols)]
     for chunk in chunks:
         print ''.join(chunk)
+    print ''
 
 
-def run(filename):
+def run(filename, part=1):
     global num_rows, num_cols, board
     with open(filename) as f:
         lines = f.read().splitlines()
@@ -47,20 +49,21 @@ def run(filename):
         # print 'rows: {} cols: {}'.format(num_rows, num_cols)
         # print 'max_position = {}'.format((num_rows-1)*num_cols+num_cols-1)
         board = ''.join(lines)
-        # pretty_print(board, num_cols)
 
+        min_occupied = 4 if part == 1 else 5
         i = 1
         while 1:
-            # print '\nRound {} of musical chairs'.format(i)
             new_board = list(board)
-            for p in range(len(board)):
-                num_occupied = count_occupied(board, p)
+            calculate_positions_for = [i for i, x in enumerate(new_board) if new_board[i] in ['#', 'L']]
+            for p in calculate_positions_for:
+                num_occupied = count_visible(board, p, 1) if part == 1 else count_visible(board, p)
                 if board[p] == 'L' and num_occupied == 0:
                     new_board[p] = '#'
-                elif board[p] == '#' and num_occupied >= 4:
+                elif board[p] == '#' and num_occupied >= min_occupied:
                     new_board[p] = 'L'
 
             # pretty_print(new_board, num_cols)
+
             if new_board == board:
                 print 'board is stable after {} rounds! {} seats occupied'.format(i, board.count('#'))
                 return board.count('#')
@@ -73,3 +76,6 @@ def run(filename):
 if __name__ == '__main__':
     assert run('testinput.txt') == 37
     run('input.txt')
+
+    assert run('testinput.txt', 2) == 26
+    run('input.txt', 2)
