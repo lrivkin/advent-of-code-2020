1ae774abc3310b2daca5a631a834f57cbc9ff437
Leah Rivkin <lrivkin@atlassian.com>
2020-12-10 22:59:27 -0800

    day 10 part 2 I hate DP

---
+++ day10/adapter_array.py
@@ -0,0 +1,46 @@
+def process(adapters):
+    difference = [adapters[i+1]-adapters[i] for i in range(len(adapters)-1)]
+    # print difference
+    num_1s = difference.count(1)
+    num_3s = difference.count(3)
+    print '1: {} 3: {}'.format(num_1s, num_3s)
+    return num_1s*num_3s
+
+
+def count_arrangements(adapters):
+    adapters.pop(0)
+    num_arrangements = {0: 1}
+    for d in adapters:
+        # print 'current adapter: {}'.format(d)
+        n = 0
+        options = [d-1, d-2, d-3]
+        # print 'options = {}'.format(options)
+        for o in options:
+            n += num_arrangements.get(o, 0)
+        # print 'sum of possible options = {}'.format(n)
+        num_arrangements[d] = n
+
+    print 'There are {} total arrangements'.format(num_arrangements[adapters[-1]])
+    return num_arrangements[adapters[-1]]
+
+
+def run(file, part):
+    with open(file) as f:
+        ratings = [int(r) for r in f.readlines()]
+        ratings.append(0)
+        ratings.append(max(ratings)+3)
+        ratings.sort()
+        if part == 1:
+            return process(ratings)
+        else:
+            return count_arrangements(ratings)
+
+
+if __name__ == '__main__':
+    assert run('test1.txt', 1) == 35
+    assert run('test2.txt', 1) == 22*10
+    print run('input.txt', 1)
+
+    assert run('test1.txt', 2) == 8
+    assert run('test2.txt', 2) == 19208
+    run('input.txt', 2)
