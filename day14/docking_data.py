c5c2d604b6a5701af92d1ccddcfcc23f7b87f3bf
Leah Rivkin <lrivkin@atlassian.com>
2020-12-14 23:34:36 -0800

    finished day 14 and refactored

--- day14/docking_data.py
+++ day14/docking_data.py
@@ -1,27 +1,52 @@
 import re
+from itertools import chain, combinations
 
 memory = {}
 
 
-def apply_mask(mask, instructions):
+def mask_value(mask, value):
+    value_binary = list(format(value, '036b'))
+    masked_value_binary = [b if m == 'X' else m for b, m in zip(value_binary, mask)]
+    return int(''.join(masked_value_binary), base=2)
+
+
+def mask_memory_address(mask, location):
+    address = list(format(location, '036b'))
+    return [b if m == '0' else m for b, m in zip(address, mask)]
+
+
+def handle_floating_bits(masked_address):
+    if 'X' not in masked_address:
+        return masked_address
+    floating_bits = [i for i, bit in enumerate(masked_address) if bit == 'X']
+    floating_bit_powerset = chain.from_iterable(
+                                    combinations(floating_bits, r)
+                                    for r in range(len(floating_bits) + 1))
+    possible_memory_locations = []
+    for option in floating_bit_powerset:
+        address_binary = ['1' if i in option else bit for i, bit in enumerate(masked_address)]
+        address_binary = [bit if bit != 'X' else '0' for bit in address_binary]
+        address = int(''.join(address_binary), base=2)
+        possible_memory_locations.append(address)
+    return possible_memory_locations
+
+
+def apply_mask(mask, instructions, version=1):
     global memory
     for i in instructions:
         tmp = i.split('=')
         location = int(re.search('[0-9]+', tmp[0]).group())
         value = int(tmp[1])
-        value_bin = list(format(value, '036b'))
-        # print(''.join(value_bin), value)
-
-        for i3, m in enumerate(mask):
-            if m != 'X':
-                value_bin[i3] = m
-        new_value = int(''.join(value_bin), base=2)
-        # print(''.join(value_bin), new_value)
-
-        memory[location] = new_value
+        if version == 1:
+            memory[location] = mask_value(mask, value)
+        else:
+            masked_address = mask_memory_address(mask, location)
+            locations = handle_floating_bits(masked_address)
+            for loc in locations:
+                memory[loc] = value
 
 
-def run(file):
+def run(file, part=1):
     global memory
     memory.clear()
     with open(file) as f:
@@ -31,9 +56,9 @@ def run(file):
             p = p.splitlines()
             mask = list(p[0])
             instructions = p[1:]
-            apply_mask(mask, instructions)
+            apply_mask(mask, instructions, part)
 
-        print(memory)
+        # print(memory)
         print('Sum of values = {}'.format(sum(memory.values())))
         return sum(memory.values())
 
@@ -41,3 +66,5 @@ def run(file):
 if __name__ == '__main__':
     assert run('test-input.txt') == 165
     run('input.txt')
+    assert run('test-input-2.txt', 2) == 208
+    run('input.txt', 2)
