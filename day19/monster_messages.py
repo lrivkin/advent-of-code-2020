fc2bc13581086a27ab02449a29e4cbdd05c58f48
Leah Rivkin <lrivkin@atlassian.com>
2020-12-23 00:40:17 -0500

    Totally brute forced the solution to day 19 part 2. At least infinite went back down to finite?

--- day19/monster_messages.py
+++ day19/monster_messages.py
@@ -9,18 +9,11 @@ def parse_rules(raw_rules):
     return rules
 
 
-def update_for_part2(rules):
-    rules['8'] = '42 | 42 8'
-    rules['11'] = '42 31 | 42 11 31'
-    return rules
-
-
-def get_rule_0(rules):
-    limit = 9
-    rule_0 = rules['0']
+def get_rule(rules, place):
+    rule_0 = rules[place]
     # split on _ to get them separated
     # if there's a |, we want to wrap in ()
-    while re.search('[0-9]', rule_0) and limit > 0:
+    while re.search('[0-9]', rule_0):
         r0 = rule_0.split()
 
         for i, r in enumerate(r0):
@@ -32,26 +25,52 @@ def get_rule_0(rules):
                 r0[i] = new_rule
 
         rule_0 = ' '.join(r0)
-        limit -= 1
+    # print(''.join(rule_0.split()))
     return ''.join(rule_0.split())
 
 
+def rule_zero_for_p2(rules, r42, r31, count8, count11):
+    rules['8'] = '({})'.format(r42) * count8
+    rules['11'] = '({})'.format(r42) * count11 + '({})'.format(r31) * count11
+    return get_rule(rules, '0')
+
+
+def count_new_matches(rules, messages):
+    # this is impossible I give up
+    rule_42 = get_rule(rules, '42')
+    print('42 matches: {}'.format(rule_42))
+    rule_31 = get_rule(rules, '31')
+    print('31 matches: {}'.format(rule_31))
+
+    # I know the current match using 42 42 31 -> yields a length of 24
+    # Max string in our set is length 48
+    # Some n number of iterations should work.......?
+    matching_message_set = set()
+    for count8 in range(1, 6):
+        for count11 in range(1, 6):
+            rule_0 = rule_zero_for_p2(rules, rule_42, rule_31, count8, count11)
+            matching_messages = [m for m in messages if re.fullmatch(rule_0, m)]
+            matching_message_set.update(set(matching_messages))
+            # print('Finding matches against {}'.format(rule_0))
+            print('number matching: {}'.format(len(matching_messages)))
+    print('{} messages match'.format(len(matching_message_set)))
+    return len(matching_message_set)
+
+
 def run(file, part=1):
-    print('\nRunning {}'.format(file))
+    print('\nRunning Part {}: {}'.format(part, file))
     with open(file) as f:
         sent = f.read().split('\n\n')
+        messages = sent[1].splitlines()
         rules = parse_rules(sent[0])
         if part == 2:
-            # this is impossible I give up
-            rules = update_for_part2(rules)
+            return count_new_matches(rules, messages)
 
-        rule_0 = get_rule_0(rules)
+        rule_0 = get_rule(rules, '0')
         print('Finding matches against {}'.format(rule_0))
-        messages = sent[1]
         num_matches = 0
-        for m in messages.splitlines():
+        for m in messages:
             if re.fullmatch(rule_0, m):
-                # print('{} matches!'.format(m))
                 num_matches += 1
         print('{} messages match'.format(num_matches))
         return num_matches
@@ -61,3 +80,6 @@ if __name__ == '__main__':
     print('--- Day 19: Monster Messages ---')
     assert run('test-input.txt') == 2
     run('input.txt')
+
+    assert run('test-input-2.txt', 2) == 12
+    run('input.txt', 2)
