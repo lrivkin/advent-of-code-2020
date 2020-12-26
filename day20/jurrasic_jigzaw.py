658e171a13ec5a5708e4639c2d2790f5ab9d3229
Leah Rivkin <lrivkin@atlassian.com>
2020-12-25 23:23:51 -0500

    Match up edges
    Find out which tile edges match to the other tiles.
    Next step: organizing them in the grid.

--- day20/jurrasic_jigzaw.py
+++ day20/jurrasic_jigzaw.py
@@ -1,9 +1,13 @@
 import re
+import pprint
+
+pp = pprint.PrettyPrinter()
 tile_store = {}     # Make printing/ visualizing easier
 
 
 def read_tiles(file):
     global tile_store
+    tile_store.clear()
     tiles = {}
     with open(file) as f:
         file_lines = f.read().split('\n\n')
@@ -21,16 +25,38 @@ def read_tiles(file):
 def get_edges(tile):
     tile_rows = tile.splitlines()
     top = tile_rows[0]
-    # print('top: {}'.format(top))
     right = ''.join([r[-1] for r in tile_rows])
-    # print('right: {}'.format(right))
     bottom = tile_rows[-1][::-1]
-    # print('bottom: {}'.format(bottom))
     left = ''.join([x[0] for x in reversed(tile_rows)])
-    # print('left: {}'.format(left))
     return [top, right, bottom, left]
+
+
+def reassemble_tiles(tiles):
+    # 1 = original direction
+    # -1 means this side was rotated somehow
+    matches = {}
+    for t in tiles:
+        for idx, e in enumerate(tiles[t]):
+            if e not in matches:
+                matches[e] = []
+            matches[e].append((t, idx, 1))
+
+            backwards_e = e[::-1]
+            if backwards_e not in matches:
+                matches[backwards_e] = []
+            matches[backwards_e].append((t, idx, -1))
+
+    matches = {k: v for k, v in matches.items() if len(v) > 1}
+    print('These tiles have edges that line up')
+    pp.pprint(matches)
 
 
 if __name__ == '__main__':
     print('--- Day 20: Jurassic Jigsaw ---')
-    read_tiles('test-input.txt')
+    # Test is only 3 x 3
+    tiles = read_tiles('test-input.txt')
+    reassemble_tiles(tiles)
+
+    # Real input is a 12 x 12
+    # tiles = read_tiles('input.txt')
+    # reassemble_tiles(tiles)
