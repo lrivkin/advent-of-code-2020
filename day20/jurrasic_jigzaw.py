77b170796f3d6e1202df945d2af05d35359e701d
Leah Rivkin <lrivkin@atlassian.com>
2020-12-26 12:58:49 -0500

    Part 1 solved
    Got the corners.
    Added printing utility to print the tiles based on a list of ids
    
    Need to actually match the tiles in their order now for part 2.

--- day20/jurrasic_jigzaw.py
+++ day20/jurrasic_jigzaw.py
@@ -1,4 +1,5 @@
 import re
+import numpy
 import pprint
 
 pp = pprint.PrettyPrinter()
@@ -18,7 +19,6 @@ def read_tiles(file):
             tiles[int(tile_id)] = get_edges(tile)
             tile_store[tile_id] = tile
     print(tiles)
-    print('')
     return tiles
 
 
@@ -45,10 +45,54 @@ def reassemble_tiles(tiles):
             if backwards_e not in matches:
                 matches[backwards_e] = []
             matches[backwards_e].append((t, idx, -1))
+    print('All matches')
+    all_matches = {}
+    # {1111: {0: (1234, 0), 1: (4567, 2)}}
+    for match_list in matches.values():
+        for tile_id, edge_id, _ in match_list:
+            if tile_id not in all_matches:
+                all_matches[tile_id] = {}
 
-    matches = {k: v for k, v in matches.items() if len(v) > 1}
-    print('These tiles have edges that line up')
-    pp.pprint(matches)
+    pp.pprint(list(matches.values()))
+
+    no_match = [v[0][0] for v in matches.values() if len(v) == 1 and v[0][2] == 1]
+    # matches = {k: v for k, v in matches.items() if len(v) > 1}
+    # print('\nThese tiles have edges that line up')
+    # pp.pprint(matches)
+    # print('\nThese edges need to be on the outside - there is no other edge that matches')
+    # pp.pprint(no_match)
+    # no_match = [tile_id for tile_id, _, orientation in no_match if orientation == 1]
+    print('\nIgnoring reverse orientation, these are the tiles/edges that do not match')
+    print(no_match)
+    corners = set()
+    prod = 1
+    for t in no_match:
+        if no_match.count(t) > 1:
+            if t not in corners:
+                corners.add(t)
+                prod *= t
+    print('Corners {}'.format(corners))
+    print('Product of corners: {}\n'.format(prod))
+
+
+def pretty_print_tiles(tile_ids=[]):
+    tile_order = list(tile_store.keys())
+    row_len = int(pow(len(tile_order), 0.5))
+    tile_size = len(tile_store[tile_order[0]].splitlines())
+
+    tile_grid = [[[] for _ in range(row_len)][:] for _ in range(row_len)]
+    for r in range(row_len):
+        tile_grid[r] = tile_order[r*row_len:(r+1)*row_len]
+
+    for i, r in enumerate(tile_grid):
+        for j, c in enumerate(r):
+            tile_grid[i][j] = tile_store[tile_grid[i][j]].splitlines()
+    print(tile_grid)
+
+    for r in tile_grid:
+        for i in range(tile_size):
+            print('  '.join([r[j][i] for j in range(row_len)]))
+        print('')
 
 
 if __name__ == '__main__':
@@ -58,5 +102,6 @@ if __name__ == '__main__':
     reassemble_tiles(tiles)
 
     # Real input is a 12 x 12
-    # tiles = read_tiles('input.txt')
-    # reassemble_tiles(tiles)
+    tiles = read_tiles('input.txt')
+    reassemble_tiles(tiles)
+
