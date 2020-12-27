36cbda6cacfdc29d47b5cc6a23eefba05255af82
Leah Rivkin <lrivkin@atlassian.com>
2020-12-27 13:43:30 -0500

    got flipping + rotating working for the tiles. Need to match everything up

--- day20/jurrasic_jigzaw.py
+++ day20/jurrasic_jigzaw.py
@@ -1,6 +1,6 @@
 import re
-import numpy
 import pprint
+from functools import reduce
 
 pp = pprint.PrettyPrinter()
 tile_store = {}     # Make printing/ visualizing easier
@@ -17,7 +17,7 @@ def read_tiles(file):
             tile_id = re.search('[0-9]+', tile_pieces[1]).group()
             tile = tile_pieces[2]
             tiles[int(tile_id)] = get_edges(tile)
-            tile_store[tile_id] = tile
+            tile_store[int(tile_id)] = tile
     print(tiles)
     return tiles
 
@@ -31,7 +31,7 @@ def get_edges(tile):
     return [top, right, bottom, left]
 
 
-def reassemble_tiles(tiles):
+def parse_tiles(tiles):
     # 1 = original direction
     # -1 means this side was rotated somehow
     matches = {}
@@ -45,34 +45,117 @@ def reassemble_tiles(tiles):
             if backwards_e not in matches:
                 matches[backwards_e] = []
             matches[backwards_e].append((t, idx, -1))
-    print('All matches')
-    all_matches = {}
-    # {1111: {0: (1234, 0), 1: (4567, 2)}}
+
+    # all_matches will be tile_id: {edge: (matching tile/edge)} format
+    all_matches = {t: {} for t in tiles}
     for match_list in matches.values():
-        for tile_id, edge_id, _ in match_list:
-            if tile_id not in all_matches:
-                all_matches[tile_id] = {}
+        a_id, a_edge, a_orientation = match_list[0]
+        # Each edge pair will have a +-1 on it, so just look at the +1 orientation
+        if a_orientation == 1:
+            if len(match_list) > 1:
+                b_id, b_edge, b_orientation = match_list[1]
+                all_matches[a_id][a_edge] = (b_id, b_edge)
+                all_matches[b_id][b_edge] = (a_id, a_edge)
 
-    pp.pprint(list(matches.values()))
+    pp.pprint(all_matches)
 
-    no_match = [v[0][0] for v in matches.values() if len(v) == 1 and v[0][2] == 1]
-    # matches = {k: v for k, v in matches.items() if len(v) > 1}
-    # print('\nThese tiles have edges that line up')
-    # pp.pprint(matches)
-    # print('\nThese edges need to be on the outside - there is no other edge that matches')
-    # pp.pprint(no_match)
-    # no_match = [tile_id for tile_id, _, orientation in no_match if orientation == 1]
     print('\nIgnoring reverse orientation, these are the tiles/edges that do not match')
-    print(no_match)
-    corners = set()
-    prod = 1
-    for t in no_match:
-        if no_match.count(t) > 1:
-            if t not in corners:
-                corners.add(t)
-                prod *= t
-    print('Corners {}'.format(corners))
+    corners = [k for k, v in all_matches.items() if len(v) == 2]
+    prod = reduce(lambda x, y: x*y, corners)
+    print('Corners: {}'.format(corners))
     print('Product of corners: {}\n'.format(prod))
+
+    solve_puzzle(all_matches, corners)
+    # edges = [k for k, v in all_matches.items() if len(v) == 3]
+    # centers = [k for k, v in all_matches.items() if len(v) == 4]
+    # print('Edges: {}'.format(edges))
+    # print('Center Pieces: {}'.format(centers))
+
+
+def flip_tile(tile_id, direction):
+    global tile_store
+
+    tile = tile_store[tile_id].splitlines()
+    if direction == 1:
+        print('flipping {} vertically'.format(tile_id))
+        flipped = tile[::-1]
+        x = '\n'.join(flipped)
+    else:
+        print('flipping {} horizontally'.format(tile_id))
+        flipped = [t[::-1] for t in tile]
+        x = '\n'.join(flipped)
+
+    print('original    flipped')
+    for i in range(10):
+        print('  '.join([tile[i], x.splitlines()[i]]))
+    print('')
+    tile_store[tile_id] = x
+
+
+def rotate_tile(tile_id, factor):
+    global tile_store
+
+    factor = factor % 4
+    if factor == 0:
+        return
+    tile = tile_store[tile_id].splitlines()
+    if factor == 1:
+        print('rotating {} to the right'.format(tile_id))
+        rotated = zip(*tile[::-1])
+        x = '\n'.join([''.join(list(r)) for r in rotated])
+    elif factor == 2:
+        print('rotating {} upside down'.format(tile_id))
+        rotated = list(zip(*tile[::-1]))[::-1]
+        x = '\n'.join([''.join(list(r)) for r in rotated])
+    elif factor == 3:
+        print('rotating {} to the left'.format(tile_id))
+        rotated = list(zip(*tile))[::-1]
+        x = '\n'.join([''.join(list(r)) for r in rotated])
+
+    print('original    rotated')
+    for i in range(10):
+        print('  '.join([tile[i], x.splitlines()[i]]))
+    print('')
+
+    tile_store[tile_id] = x
+
+
+def rotate_corner(corner_id, edge_matches, orientation):
+    if edge_matches.keys() == orientation:
+        return corner_id, edge_matches
+    # both of these could be {0,1}, {1,2}, {2,3}, {3,0}
+    edge1 = 3 if orientation == {3, 0} else min(orientation)
+    edge2 = (edge1 + 1) % 4
+
+    m1 = 3 if edge_matches.keys() == {3, 0} else min(edge_matches.keys())
+    m2 = (m1 + 1) % 4
+
+    # Build the new immediate tile
+    new_match = {
+        edge1: edge_matches[m1],
+        edge2: edge_matches[m2]
+    }
+    # TEST OUT ROTATION/ FLIPPING:
+    rotate_tile(corner_id, 1)
+    rotate_tile(corner_id, 2)
+    rotate_tile(corner_id, 3)
+    flip_tile(corner_id, 1)
+    flip_tile(corner_id, -1)
+    return corner_id, new_match
+
+
+def solve_puzzle(match_map, corners):
+    num_tiles = len(match_map)
+    square_len = int(pow(num_tiles, 0.5))
+    tile_grid = [[0 for _ in range(square_len)][:] for _ in range(square_len)]
+
+    match_map_corners = {c: match_map[c] for c in corners}
+    # Set the first corner (doesn't matter what I pick)
+    top_left_id = corners[0]
+    top_left_edges = match_map_corners[top_left_id]
+    print(top_left_id, top_left_edges)
+    top_left_id, top_left_edges = rotate_corner(top_left_id, top_left_edges, {1, 2})
+    print(top_left_id, top_left_edges)
 
 
 def pretty_print_tiles(tile_ids=[]):
@@ -99,9 +182,9 @@ if __name__ == '__main__':
     print('--- Day 20: Jurassic Jigsaw ---')
     # Test is only 3 x 3
     tiles = read_tiles('test-input.txt')
-    reassemble_tiles(tiles)
+    parse_tiles(tiles)
 
     # Real input is a 12 x 12
-    tiles = read_tiles('input.txt')
-    reassemble_tiles(tiles)
+    # tiles = read_tiles('input.txt')
+    # parse_tiles(tiles)
 
