3c9eff1e727cc3860c14122ef957f3f569d7aff6
Leah Rivkin <lrivkin@atlassian.com>
2020-12-29 18:49:29 -0500

    Part 2/ Part 1 Refactor
    Cleaned up part 1 (sort of) but left the inefficiencies there.
    Solved part 2. Did it with a sort of singly linked list, changing
    the next value in place.

--- day23/crab_cups.py
+++ day23/crab_cups.py
@@ -1,16 +1,4 @@
 max_cup = 0
-len_cups = 0
-
-
-def starter_cups(start, total_cups):
-    global max_cup, len_cups
-    cups = [int(i) for i in list(start)]
-    more_cups = total_cups - len(cups)
-    next_cup_val = max(cups) + 1
-    cups = cups + list(range(next_cup_val, next_cup_val + more_cups))
-    max_cup = max(cups)
-    len_cups = len(cups)
-    return cups
 
 
 def get_destination(current_value, moving):
@@ -23,57 +11,112 @@ def get_destination(current_value, moving):
         destination -= 1
 
 
-def play_game(start, part=1):
-    max_cups = len(start) if part == 1 else 1000000
-    turns = 100 if part == 1 else 10000000
-    print('\nStart: {} max_cups={}, turns={}'.format(start, max_cups, turns))
+def part2(start):
+    global max_cup
+    max_cup = 1000000
+    turns = 10000000
+    # cups will represent the value of the cup
+    # the value IN the cup will be the next cup
+    start = [int(v) for v in start]
+    cups = [0 for _ in range(max_cup+1)]
 
-    cups = starter_cups(start, max_cups)
+    curr_cup = start[0]
+    cups[-1] = start[0]
+    i = 0
+    while 1:
+        if i == len(start)-1:
+            cups[next_val] = i+2
+            break
+        val = start[i]
+        next_val = start[i+1]
+        cups[val] = next_val
+        i += 1
+
+    for i in range(len(start)+1, max_cup):
+        cups[i] = i+1
+
+    # print([(i, cups[i]) for i in range(1, 10)])
+
+    for _ in range(turns):
+        n1 = cups[curr_cup]
+        n2 = cups[n1]
+        n3 = cups[n2]
+        dest = get_destination(curr_cup, [n1, n2, n3])
+        # print(f'destination: {dest}')
+
+        # destination = 2. 2-> next now = 5
+        # after we move.... 2 -> 8 -> 9 -> 1 -> 5
+        next_cup = cups[n3]
+        cups[curr_cup] = cups[n3]
+
+        cups[n3] = cups[dest]
+        cups[dest] = n1
+
+        curr_cup = next_cup
+        # print(f'after moving pointing at {next_cup}')
+        # print(n1, n2, n3)
+        # print([(i, cups[i]) for i in range(1, 10)])
+
+    val1 = cups[1]
+    val2 = cups[val1]
+    print(val1, val2)
+    print(f'multiplied together = {val1*val2}')
+
+
+def play_game(start):
+    global max_cup
+    max_cups = len(start)
+    turns = 100
+
+    print('Start: {} max_cups={}, turns={}'.format(start, max_cups, turns))
+
+    cups = [int(i) for i in list(start)]
+    max_cup = max(cups)
     current_idx = 0
     for move in range(turns):
-        if move % 1000 == 0:
-            print('Move {}, index: {}'.format(move, current_idx))
-
         current_value = cups[current_idx]
-        next_value = cups[(current_idx+4) % len_cups]
+        next_value = cups[(current_idx+4) % max_cups]
         # print('my cup: {}'.format(current_value))
         # print('cups: {}'.format(cups))
 
-        # next_indices = [(current_idx+1+r) % len_cups for r in range(3)]
-        # moving = [cups[i] for i in next_indices]
+        # O(1) to calculate/ get the ones to move
+        moving_indices = [(current_idx + i) % len(cups) for i in range(1, 4)]
+        moving = [cups[i] for i in moving_indices]
+        # print(f'pick up: {moving}')
 
-        # .pop() is O(N-i) -> O(N)
-        moving = [cups.pop((current_idx + 1) % len(cups)) for _ in range(3)]
-
-        # print('pick up: {}'.format(moving))
+        # O(N) for each deletion
+        for i in sorted(moving_indices, reverse=True):
+            del cups[i]
 
         # O(1)
         destination_val = get_destination(current_value, moving)
+        # print(f'destination: {destination_val}')
 
+        # O(N) to search for a value
         destination_idx = cups.index(destination_val)+1
+
         # Each insert is O(N)
         cups.insert(destination_idx, moving[0])
         cups.insert(destination_idx+1, moving[1])
         cups.insert(destination_idx+2, moving[2])
 
         # O(N) operation
-        # Would be able to get this down to O(1) with a bit of math which could be good
         current_idx = cups.index(next_value)
 
     # print('final cups: {}'.format(cups))
     idx_1 = cups.index(1)
-    if part == 1:
-        after_1 = cups[idx_1+1:] + cups[:idx_1]
-        print('After cup 1: {}'.format(''.join([str(i) for i in after_1])))
-    else:
-        cup_a = cups[(idx_1+1) % len_cups]
-        cup_b = cups[(idx_1+2) % len_cups]
-        print('Cup 1: {}, Cup 2: {}, multiplied: {}'.format(cup_a, cup_b, cup_a*cup_b))
+    after_1 = cups[idx_1+1:] + cups[:idx_1]
+    print('After cup 1: {}\n'.format(''.join([str(i) for i in after_1])))
 
 
 if __name__ == '__main__':
     print('--- Day 23: Crab Cups ---')
-    # play_game('389125467')
-    # play_game('916438275')
+    print('Part 1')
+    play_game('389125467')
+    play_game('916438275')
 
-    play_game('389125467', 2)
+    print('\nPart 2')
+    print('Test input: 389125467')
+    part2('389125467')
+    print('\nReal input: 916438275')
+    part2('916438275')
